b4702ad0fbfc6d55df365cab282a1fe14d4b6946
Serhii_Kudryk <sergiy.kudryk@gmail.com>
2025-09-28 18:44:49 +0300

    rewrite client to Boom Now APi to fix misc. issues

--- BUGS.md
+++
@@ -1,6 +0,0 @@
-**Bugs:**
-
-- 1) API request to https://app.boomnow.com/open_api/v1/auth/token, using params: {"client_id": "boom_3a21..4814", "client_secret": "76df8...7c76"}
-failed to return JSON response but instead return a HTML page with next error message in the body:
-
-    _We're sorry but dvr-dashboard doesn't work properly without JavaScript enabled. Please enable it to continue._
--- app/services/external/client.rb
+++ app/services/external/client.rb
@@ -4,29 +4,18 @@ class External::Client
   class AuthenticationError < Error; end
   class NotFoundError < Error; end
 
-  attr_accessor :token
+  attr_accessor :token, :connection,  :skip_auth_header
 
   def initialize
-    @connection = Faraday.new(
-      url: ExternalApi::BASE_URL,
-      headers: {
-        'Content-Type' => 'application/json',
-        'Accept' => 'application/json',
-        'Authorization' => auth_header
-      }
-    ) do |conn|
-      conn.request :json
-      conn.response :json
-      conn.adapter Faraday.default_adapter
-    end
-
     fetch_auth_token
+    @skip_auth_header = false
   end
 
   def get(path, params = {})
-    p "API request to #{ExternalApi::BASE_URL}#{path}, using params: #{params}"
+    url = "#{ExternalApi::BASE_URL}#{path}"
+    Rails.logger.debug "-- GET API request to #{url}, using params: #{params}"
 
-    response = @connection.get(path, params)
+    response = connection.get(url, params)
     handle_response(response)
   rescue Faraday::TimeoutError => e
     raise TimeoutError, "Request timed out: #{e.message}"
@@ -34,28 +23,45 @@ class External::Client
     raise Error, "Connection failed: #{e.message}"
   end
 
-  private
+  def post(path, params = {})
+    url = "#{ExternalApi::BASE_URL}#{path}"
+    Rails.logger.debug "-- POST API request to #{url}, using params: #{params}"
+    response = connection.post(url, params.to_json)
+    handle_response(response)
+  rescue Faraday::TimeoutError => e
+    raise TimeoutError, "Request timed out: #{e.message}"
+  rescue Faraday::ConnectionFailed => e
+    raise Error, "Connection failed: #{e.message}"
+  end
+
+private
+
+  def connection
+      @connection ||=
+        Faraday.new(url: ExternalApi::BASE_URL,  headers: ) do |conn|
+          conn.request :json
+          conn.response :json
+          conn.adapter Faraday.default_adapter
+        end
+  end
+
+  def token(refresh: false)
+    @token && !refresh ? @token :  fetch_auth_token
+  end
 
   def fetch_auth_token
-    resp = get('/auth/token', {client_id: ExternalApi::CLIENT_ID, client_secret: ExternalApi::CLIENT_SECRET})
-    # FIXME - got HTML response
-    return nil unless resp.respond_to?(:status)
-
-    case resp.status
-    when 200..299
-      @token = resp.body["token"]
-    when 401
-      raise AuthenticationError, 'Invalid credentials'
-    end
-  end
-  
-  def auth_header
-    "Bearer #{@token}"
+    @skip_auth_header = true
+    payload = {client_id: ExternalApi::CLIENT_ID, client_secret: ExternalApi::CLIENT_SECRET}
+    body = post('/auth/token', payload)
+    @skip_auth_header = false
+    @connection = nil # reset to refresh headers
+    @token = body["access_token"]
   end
 
   def handle_response(response)
-    p "API response: #{response.status}"
-    p "API response: #{response.body}"
+    Rails.logger.debug "-- API response status: #{response.status}"
+    Rails.logger.debug "-- API response body: #{response.body}"
+    Rails.logger.debug "\n"
 
     case response.status
     when 200..299
@@ -72,5 +78,19 @@ class External::Client
       raise Error, "Unexpected response: #{response.status} - #{response.body}"
     end
   end
+
+  def headers
+    out = {
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      }
+    out['Authorization'] = auth_header unless @skip_auth_header
+    out
+  end
+
+  def auth_header
+    "Bearer #{@token}"
+  end
 end
+
 
